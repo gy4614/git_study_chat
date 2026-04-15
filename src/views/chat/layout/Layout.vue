1bc95e9db7e07d6624e6d76c3959ca34d91e541d
gy4614 <2030488832@qq.com>
2026-04-15 21:39:31 +0800

    页面基本骨架

---
+++ src/views/chat/layout/Layout.vue
@@ -0,0 +1,57 @@
+<!--  页面骨架 + 路由容器 + 权限控制中心
+  1.左侧栏 + 主内容区
+  2. 根据路由切换聊天内容
+  3. 控制登录 / 权限弹窗
+Permission 👉 权限弹窗（登录提示）
+-->
+<script setup lang='ts'>
+import { computed } from 'vue'
+import { NLayout, NLayoutContent } from 'naive-ui'
+import { useRouter } from 'vue-router'
+import Sider from './sider/index.vue'
+import Permission from './Permission.vue' 
+import { useBasicLayout } from '@/hooks/useBasicLayout'
+import { useAppStore, useAuthStore, useChatStore } from '@/store'
+
+const router = useRouter()
+const appStore = useAppStore()
+const chatStore = useChatStore()
+const authStore = useAuthStore()
+// 页面加载时：自动跳到当前激活的聊天
+router.replace({ name: 'Chat', params: { uuid: chatStore.active } })
+
+const { isMobile } = useBasicLayout()
+
+const collapsed = computed(() => appStore.siderCollapsed)
+
+const needPermission = computed(() => !!authStore.session?.auth && !authStore.token)
+
+const getMobileClass = computed(() => {
+  if (isMobile.value)
+    return ['rounded-none', 'shadow-none']
+  return ['border', 'rounded-md', 'shadow-md', 'dark:border-neutral-800']
+})
+
+const getContainerClass = computed(() => {
+  return [
+    'h-full',
+    { 'pl-[260px]': !isMobile.value && !collapsed.value },
+  ]
+})
+</script>
+
+<template>
+  <div class="h-full dark:bg-[#24272e] transition-all" :class="[isMobile ? 'p-0' : 'p-4']">
+    <div class="h-full overflow-hidden" :class="getMobileClass">
+      <NLayout class="z-40 transition" :class="getContainerClass" has-sider>
+        <Sider />
+        <NLayoutContent class="h-full">
+          <RouterView v-slot="{ Component, route }">
+            <component :is="Component" :key="route.fullPath" />
+          </RouterView>
+        </NLayoutContent>
+      </NLayout>
+    </div>
+    <Permission :visible="needPermission" />
+  </div>
+</template>
